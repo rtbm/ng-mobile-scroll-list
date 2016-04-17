862722d8fae80a066d6295a4d8f4b00fbdcf33c2
rtbm <marcin@bogusz.cc>
2016-04-17 23:36:42 +0200

    perf WIP

--- src/components/list/javascript/list-component.js
+++ src/components/list/javascript/list-component.js
@@ -8,19 +8,19 @@ const listComponent = {
   controller($element, $scope, $timeout) {
     'ngInject';
     this.activeIndex = 0;
-    this.debouncer = false;
+
+    const maxIndex = this.items.length - 1;
+    let debouncer = false;
+
+    const setActiveItem = () => {
+      const target = Math.floor($element[0].scrollTop / 65);
+      this.activeIndex = target < maxIndex ? target : maxIndex;
+      debouncer = false;
+    };
 
     $element.bind('scroll', () => {
-      if(this.debouncer) {
-        $timeout.cancel(this.debouncer);
-      }
-
-      this.debouncer = $timeout(() => $scope.$apply(() => {
-        const target = Math.floor($element[0].scrollTop / 65);
-        const maxIndex = this.items.length - 1;
-        this.activeIndex = target < maxIndex ? target : maxIndex;
-        this.debouncer = false;
-      }), 10);
+      if(debouncer) { $timeout.cancel(debouncer); }
+      debouncer = $timeout(() => setActiveItem(), 5);
     });
 
     $scope.$on('destroy', () => $element.unbind('scroll'));
